fefb15753f8f31dd816aa62773710eeeb981e95b
Calvin Remsburg <cremsburg.dev@gmail.com>
2023-04-05 07:17:01 -0500

    s3 backend

--- san-vfw-01/provider.tf
+++ san-vfw-01/provider.tf
@@ -15,3 +15,9 @@ provider "panos" {
   username = var.panos_username
   password = var.panos_password
 }
+
+provider "aws" {
+  region     = "us-east-1"
+  access_key = var.aws_access_key
+  secret_key = var.aws_secret_key
+}
--- san-vfw-01/variables.tf
+++ san-vfw-01/variables.tf
@@ -10,6 +10,14 @@ variable "panos_password" {
   description = "Password of user"
 }
 
+variable "aws_access_key" {
+  description = "AWS access key"
+}
+
+variable "aws_secret_key" {
+  description = "AWS secret key"
+}
+
 variable "service_objects" {
   description = "List of service objects to be created"
   type = list(object({
